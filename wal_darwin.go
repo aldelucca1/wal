4a7a4c561339975eb469a4a06d77ac537c48882a
Al DeLucca <adelucca@flowcode.com>
2023-02-10 05:21:37 -0500

    fix: Avoid unnecessary allocation when reading

--- wal_darwin.go
+++ wal_darwin.go
@@ -6,19 +6,23 @@ import (
 	"syscall"
 )
 
+// NewWriter returns a direct io file to the specified path
 func NewWriter(name string, flag int, perm os.FileMode) (File, error) {
 	return openFile(name, flag, perm)
 }
 
+// NewReader returns a direct io file to the specified path
 func NewReader(name string, flag int, perm os.FileMode) (File, error) {
 	return openFile(name, flag, perm)
 }
 
-func openFile(name string, flag int, perm os.FileMode) (file *os.File, err error) {
+// openFile opens a file disabling file system caching
+func openFile(name string, flag int, perm os.FileMode) (*os.File, error) {
 
-	file, err = os.OpenFile(name, flag, perm)
+	// Open the file as usual
+	file, err := os.OpenFile(name, flag, perm)
 	if err != nil {
-		return
+		return file, err
 	}
 
 	// Set F_NOCACHE to avoid caching
@@ -27,9 +31,8 @@ func openFile(name string, flag int, perm os.FileMode) (file *os.File, err error
 	//              on.
 	_, _, e1 := syscall.Syscall(syscall.SYS_FCNTL, uintptr(file.Fd()), syscall.F_NOCACHE, 1)
 	if e1 != 0 {
-		err = fmt.Errorf("failed to set F_NOCACHE: %s", e1)
 		file.Close()
-		file = nil
+		return nil, fmt.Errorf("failed to set F_NOCACHE: %s", e1)
 	}
-	return
+	return file, nil
 }
--- wal_linux.go
+++ wal_linux.go
@@ -11,34 +11,42 @@ import (
 	"github.com/brk0v/directio"
 )
 
+// DirectFile wraps direct access to the underlying file
 type DirectFile struct {
 	file *os.File
-	*directio.DirectIO
+	buf  *directio.DirectIO
 }
 
+// Close flushes the direct io buffer and closes the underlying file
 func (d *DirectFile) Close() error {
-	if err := d.Flush(); err != nil {
+	if err := d.buf.Flush(); err != nil {
 		return err
 	}
 	return d.file.Close()
 }
 
+// Read is unimplemented
 func (d *DirectFile) Read(p []byte) (n int, err error) {
 	return 0, errors.New("unimplemented")
 }
 
+// Write writes to the direct io buffer
 func (d *DirectFile) Write(p []byte) (n int, err error) {
-	return d.DirectIO.Write(p)
+	return d.buf.Write(p)
 }
 
+// Seek seeks the underlying file to the specified location
 func (d *DirectFile) Seek(offset int64, whence int) (int64, error) {
 	return d.file.Seek(offset, whence)
 }
 
+// Sync flushes the direct io buffer
 func (d *DirectFile) Sync() error {
-	return d.Flush()
+	return d.buf.Flush()
 }
 
+// NewWriter creates a new direct io file with the specified name, flags, and
+// permissions
 func NewWriter(name string, flag int, perm os.FileMode) (File, error) {
 	file, err := os.OpenFile(name, flag|syscall.O_DIRECT, perm)
 	if err != nil {
@@ -51,6 +59,8 @@ func NewWriter(name string, flag int, perm os.FileMode) (File, error) {
 	return &DirectFile{file, reader}, err
 }
 
+// NewReader creates a new direct io file with the specified name, flags, and
+// permissions
 func NewReader(name string, flag int, perm os.FileMode) (File, error) {
 	return os.OpenFile(name, flag|syscall.O_DIRECT, perm)
 }
